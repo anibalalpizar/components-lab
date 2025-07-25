b69791071a309593c0697aad7a59a33cdc7c28e6
anibalalpizar <anibal.alpizar14@gmail.com>
2025-07-25 05:40:11 -0600

    Reorder imports for improved organization in CodePreview.tsx

--- src/components/CodePreview.tsx
+++ src/components/CodePreview.tsx
@@ -1,9 +1,10 @@
 "use client"
 
-import type { EditorComponent } from "../types/editor"
-import { generateTSXCode } from "../utils/codeGenerator"
 import { Prism as SyntaxHighlighter } from "react-syntax-highlighter"
 import { oneDark } from "react-syntax-highlighter/dist/esm/styles/prism"
+
+import type { EditorComponent } from "../types/editor"
+import { generateTSXCode } from "../utils/codeGenerator"
 
 interface CodePreviewProps {
   components: EditorComponent[]
